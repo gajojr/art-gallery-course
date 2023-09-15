79e73a2e5b45df1725239fde9af95443bf20ceb9
gajojr <andrijagajicbusiness@gmail.com>
2023-09-16 00:10:05 +0200

    chore: add sing in link on the sign up screen

--- components/SignUpPageComponents/LogInLink/LogInLink.component.tsx
+++ components/SignUpPageComponents/LogInLink/LogInLink.component.tsx
@@ -1,11 +1,34 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+	ComponentWrapper,
+	LinkText,
+	LinkWrapper,
+	PreText,
+} from './LogInLink.style';
+import {
+	useFonts,
+	Poppins_300Light,
+	Poppins_400Regular,
+} from '@expo-google-fonts/poppins';
+import { useNavigation } from '@react-navigation/native';
 
 const LogInLink = () => {
+	const navigation = useNavigation();
+	const [loaded, error] = useFonts({
+		Poppins_300Light,
+		Poppins_400Regular,
+	});
+
+	if (!loaded || error) {
+		return <></>;
+	}
+
 	return (
-		<View>
-			<Text>LogInLink.component</Text>
-		</View>
+		<ComponentWrapper>
+			<PreText>Already have an account?</PreText>
+			<LinkWrapper onPress={() => navigation.navigate('SignIn' as never)}>
+				<LinkText>Log In</LinkText>
+			</LinkWrapper>
+		</ComponentWrapper>
 	);
 };
 
