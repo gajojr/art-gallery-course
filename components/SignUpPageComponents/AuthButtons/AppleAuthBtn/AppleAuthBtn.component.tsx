924c9d7df33b2adfefb73c81b83911405f84dc55
gajojr <andrijagajicbusiness@gmail.com>
2023-09-15 23:47:23 +0200

    auth: add sign up with apple button on sign up screen

--- components/SignUpPageComponents/AuthButtons/AppleAuthBtn/AppleAuthBtn.component.tsx
+++ components/SignUpPageComponents/AuthButtons/AppleAuthBtn/AppleAuthBtn.component.tsx
@@ -1,11 +1,64 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { useOAuth } from '@clerk/clerk-expo';
+import { useWarmUpBrowser } from '../../../../hooks/useWarmUpBrowser';
+import { ButtonWrapper, Icon } from '../AuthButtons.style';
+import { useNavigation } from '@react-navigation/native';
+import { doc, setDoc } from 'firebase/firestore';
+import db from '../../../../firebase-config';
+import { useDispatch } from 'react-redux';
+import {
+	selectAuthType,
+	selectAuthenticated,
+	selectEmailAddress,
+	selectFullname,
+} from '../../../../redux/reducers/Auth';
+
+WebBrowser.maybeCompleteAuthSession();
 
 const AppleAuthBtn = () => {
+	useWarmUpBrowser();
+	const dispatch = useDispatch();
+	const navigation = useNavigation();
+	const { startOAuthFlow } = useOAuth({
+		strategy: 'oauth_apple',
+	});
+
+	const appleSignup = async () => {
+		try {
+			const { createdSessionId, setActive, signUp } = await startOAuthFlow();
+
+			if (createdSessionId && setActive) {
+				setActive({ session: createdSessionId });
+
+				await setDoc(doc(db, 'users', signUp?.emailAddress as string), {
+					fullname: '',
+					emailAddress: signUp?.emailAddress,
+					username: '',
+					profileImgUrl: '',
+					authType: 'apple',
+					creationDate: new Date(),
+				});
+
+				dispatch(selectAuthType('apple'));
+				dispatch(selectAuthenticated(true));
+				dispatch(selectEmailAddress(signUp?.emailAddress));
+				dispatch(selectFullname(''));
+
+				navigation.navigate('Profile' as never);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
-		<View>
-			<Text>AppleAuthBtn.component</Text>
-		</View>
+		<ButtonWrapper onPress={appleSignup}>
+			<Icon
+				name='apple1'
+				color='#fff'
+				size={30}
+			/>
+		</ButtonWrapper>
 	);
 };
 
