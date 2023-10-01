eb6ab3cb93b341a3a95c79683b360d0e63a39bdd
gajojr <andrijagajicbusiness@gmail.com>
2023-10-01 09:32:03 +0200

    test: add unit tests for AppleAuthBtn component on sign in screen

---
+++ components/SignInScreenComponents/AuthButtons/AppleAuthBtn/AppleAuthBtn.test.tsx
@@ -0,0 +1,199 @@
+import { render, fireEvent, act, waitFor } from '../../../../test-utils';
+import AppleAuthBtn from './AppleAuthBtn.component';
+import { useOAuth, useUser } from '@clerk/clerk-expo';
+import { doc, getDoc } from 'firebase/firestore';
+import { Alert } from 'react-native';
+
+jest.mock('react-native', () => {
+	const rn = jest.requireActual('react-native');
+	rn.Alert.alert = jest.fn();
+	return rn;
+});
+
+jest.mock('@clerk/clerk-expo', () => ({
+	useOAuth: jest.fn(),
+	useUser: jest.fn(),
+}));
+
+jest.mock('firebase/firestore', () => ({
+	getDoc: jest.fn(),
+	doc: jest.fn(),
+	getFirestore: jest.fn(),
+}));
+
+jest.mock('firebase/app', () => ({
+	initializeApp: jest.fn(),
+}));
+
+jest.mock('@react-navigation/native', () => ({
+	...jest.requireActual('@react-navigation/native'),
+	useNavigation: jest.fn(),
+}));
+
+jest.mock('react-redux', () => ({
+	...jest.requireActual('react-redux'),
+	useDispatch: () => jest.fn(),
+}));
+
+describe('AppleAuthBtn', () => {
+	afterEach(() => {
+		jest.resetAllMocks();
+	});
+
+	it('should iniate OAuth flow when AppleAuthBtn button is clicked', async () => {
+		const startOAuthFlowMock = jest.fn().mockResolvedValue({
+			createdSessionId: 'test-session-id',
+			setActive: jest.fn(),
+		});
+
+		(useOAuth as jest.Mock).mockReturnValue({
+			startOAuthFlow: startOAuthFlowMock,
+		});
+		(useUser as jest.Mock).mockReturnValue({
+			user: {
+				primaryEmailAddress: {
+					emailAddress: 'example@gmail.com',
+				},
+			},
+		});
+
+		const { getByTestId } = render(<AppleAuthBtn />);
+
+		const button = getByTestId('Btn');
+
+		await act(async () => {
+			fireEvent.press(button);
+		});
+
+		expect(startOAuthFlowMock).toHaveBeenCalledTimes(1);
+	});
+
+	it('should sign in successfully with Apple OAuth', async () => {
+		const startOAuthFlowMock = jest.fn().mockResolvedValue({
+			createdSessionId: 'test-session-id',
+			setActive: jest.fn(),
+		});
+
+		(doc as jest.Mock).mockReturnValue('users');
+		(getDoc as jest.Mock).mockResolvedValue({
+			exists: () => true,
+			data: () => ({
+				fullname: 'Test User',
+				emailAddress: 'test@example.com',
+				username: 'testuser',
+				profileImgUrl: '',
+				authType: 'apple',
+			}),
+		});
+		(useOAuth as jest.Mock).mockReturnValue({
+			startOAuthFlow: startOAuthFlowMock,
+		});
+		(useUser as jest.Mock).mockReturnValue({
+			user: {
+				primaryEmailAddress: {
+					emailAddress: 'test@example.com',
+				},
+			},
+		});
+
+		const { getByTestId } = render(<AppleAuthBtn />);
+
+		const button = getByTestId('Btn');
+		await act(async () => {
+			fireEvent.press(button);
+		});
+
+		expect(getDoc).toHaveBeenLastCalledWith('users');
+	});
+
+	it('should handle failure to start OAuth flow', async () => {
+		const error = new Error('Failed to start OAuth flow');
+		const startOAuthFlowMock = jest.fn().mockRejectedValue(error);
+		(useOAuth as jest.Mock).mockReturnValue({
+			startOAuthFlow: startOAuthFlowMock,
+		});
+		(useUser as jest.Mock).mockReturnValue({
+			user: {
+				primaryEmailAddress: {
+					emailAddress: 'test@example.com',
+				},
+			},
+		});
+
+		const { getByTestId } = render(<AppleAuthBtn />);
+
+		const button = getByTestId('Btn');
+		fireEvent.press(button);
+
+		expect(startOAuthFlowMock).toHaveBeenCalledTimes(1);
+
+		await waitFor(() => {
+			expect(Alert.alert).toHaveBeenCalledWith('Error occurred, try again');
+		});
+	});
+
+	it('should handle failure to set the session', async () => {
+		const error = new Error('Failed to set the session');
+		const startOAuthFlowMock = jest.fn().mockResolvedValue({
+			createdSessionId: 'some-session-id',
+			setActive: jest.fn().mockImplementation(() => {
+				throw error;
+			}),
+		});
+		(useOAuth as jest.Mock).mockReturnValue({
+			startOAuthFlow: startOAuthFlowMock,
+		});
+		(useUser as jest.Mock).mockReturnValue({
+			user: {
+				primaryEmailAddress: {
+					emailAddress: 'test@example.com',
+				},
+			},
+		});
+
+		const { getByTestId } = render(<AppleAuthBtn />);
+
+		const button = getByTestId('Btn');
+		fireEvent.press(button);
+
+		expect(startOAuthFlowMock).toHaveBeenCalledTimes(1);
+
+		await waitFor(() => {
+			expect(Alert.alert).toHaveBeenCalledWith('Error occurred, try again');
+		});
+	});
+
+	it('should handle failure to get user data from Firestore', async () => {
+		const error = new Error('Failed to get user data');
+		(getDoc as jest.Mock).mockRejectedValue(error);
+		jest.spyOn(console, 'error').mockImplementation(() => {});
+
+		const startOAuthFlowMock = jest.fn().mockResolvedValue({
+			createdSessionId: 'some-session-id',
+			setActive: jest.fn(),
+		});
+		(useOAuth as jest.Mock).mockReturnValue({
+			startOAuthFlow: startOAuthFlowMock,
+		});
+		(useUser as jest.Mock).mockReturnValue({
+			user: {
+				primaryEmailAddress: {
+					emailAddress: 'test@example.com',
+				},
+			},
+		});
+
+		const { getByTestId } = render(<AppleAuthBtn />);
+
+		const button = getByTestId('Btn');
+		await act(() => {
+			fireEvent.press(button);
+		});
+
+		expect(getDoc).toHaveBeenCalledTimes(1);
+		expect(console.error).toHaveBeenCalledWith(
+			'Failed to get user data:',
+			error
+		);
+	});
+});
