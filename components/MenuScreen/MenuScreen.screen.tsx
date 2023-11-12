6d9f1f1309fc8ae46cf821588862b904153aebf4
gajojr <andrijagajicbusiness@gmail.com>
2023-11-12 21:16:28 +0100

    feat: add side menu for navigation

--- components/MenuScreen/MenuScreen.screen.tsx
+++ components/MenuScreen/MenuScreen.screen.tsx
@@ -1,17 +1,143 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { Container } from './MenuScreen.style';
+import { Alert, TouchableOpacity, View } from 'react-native';
+import { useAuth } from '@clerk/clerk-expo';
+import {
+	AccountIcon,
+	GalleryIcon,
+	HomeIcon,
+	LinkText,
+	LinkWrapper,
+	LogutIcon,
+	MenuContainer,
+	MenuIcon,
+} from './MenuScreen.style';
 import { DrawerNavigationHelpers } from '@react-navigation/drawer/lib/typescript/src/types';
+import { useFonts, Poppins_300Light } from '@expo-google-fonts/poppins';
+import { useDispatch } from 'react-redux';
+import { AuthType } from '../../redux/types/Auth';
+import {
+	selectAuthType,
+	selectAuthenticated,
+	selectBio,
+	selectEmailAddress,
+	selectFullname,
+	selectInstagram,
+	selectProfileImgUrl,
+	selectTwitter,
+	selectUsername,
+	selectWebsite,
+} from '../../redux/reducers/Auth';
 
 const MenuScreen = ({
 	navigation,
 }: {
 	navigation: DrawerNavigationHelpers;
 }) => {
+	const { isLoaded, signOut } = useAuth();
+	const dispatch = useDispatch();
+	const [loaded, error] = useFonts({
+		Poppins_300Light,
+	});
+
+	if (!loaded || error || !isLoaded) {
+		return <></>;
+	}
+
 	return (
-		<Container>
-			<Text>MenuScreen.screen</Text>
-		</Container>
+		<MenuContainer>
+			<View
+				style={{
+					flexDirection: 'row',
+					width: '100%',
+					justifyContent: 'space-between',
+					padding: 20,
+				}}
+			>
+				<View></View>
+				<TouchableOpacity
+					onPress={() => {
+						navigation.closeDrawer();
+					}}
+				>
+					<MenuIcon source={require('../../assets/images/menu-close.png')} />
+				</TouchableOpacity>
+			</View>
+			<LinkWrapper
+				onPress={() => {
+					navigation.navigate('Gallery');
+				}}
+			>
+				<HomeIcon
+					name='home'
+					size={25}
+					color='#fff'
+				/>
+				<LinkText>Gallery</LinkText>
+			</LinkWrapper>
+			<LinkWrapper
+				onPress={() => {
+					navigation.navigate('Profile');
+				}}
+			>
+				<AccountIcon
+					name='account'
+					size={25}
+					color='#fff'
+				/>
+				<LinkText>Profile</LinkText>
+			</LinkWrapper>
+			<LinkWrapper
+				onPress={() => {
+					navigation.navigate('MyArt');
+				}}
+			>
+				<GalleryIcon
+					name='image'
+					size={25}
+					color='#fff'
+				/>
+				<LinkText>My art</LinkText>
+			</LinkWrapper>
+			<LinkWrapper
+				onPress={() => {
+					Alert.alert(
+						'Log Out',
+						'Are you sure you want to log out?',
+						[
+							{
+								text: 'Cancel',
+								onPress: () => console.log('Cancel Pressed'),
+								style: 'cancel',
+							},
+							{
+								text: 'Yes',
+								onPress: () => {
+									signOut();
+									dispatch(selectAuthenticated(false));
+									dispatch(selectAuthType(AuthType.EMAIL));
+									dispatch(selectUsername(''));
+									dispatch(selectEmailAddress(''));
+									dispatch(selectProfileImgUrl(''));
+									dispatch(selectFullname(''));
+									dispatch(selectBio(''));
+									dispatch(selectTwitter(''));
+									dispatch(selectInstagram(''));
+									dispatch(selectWebsite(''));
+									navigation.navigate('Home');
+								},
+							},
+						],
+						{ cancelable: false }
+					);
+				}}
+			>
+				<LogutIcon
+					name='log-out-outline'
+					size={25}
+					color='#fff'
+				/>
+				<LinkText>Log out</LinkText>
+			</LinkWrapper>
+		</MenuContainer>
 	);
 };
 
--- components/MenuScreen/MenuScreen.style.ts
+++ components/MenuScreen/MenuScreen.style.ts
@@ -1,3 +1,37 @@
 import styled from 'styled-components/native';
+import OpticonIcon from 'react-native-vector-icons/Octicons';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 
-export const Container = styled.View``;
+export const MenuContainer = styled.SafeAreaView`
+	flex: 1;
+	background-color: #222;
+	padding: 20px;
+	background-color: #242424;
+`;
+
+export const MenuIcon = styled.Image.attrs({
+	testID: 'MenuIcon',
+})``;
+
+export const LinkWrapper = styled.TouchableOpacity`
+	width: 100%;
+	flex-direction: row;
+	padding: 10px;
+	align-items: center;
+`;
+
+export const LinkText = styled.Text`
+	color: rgba(255, 255, 255, 0.85);
+	font-family: Poppins_300Light;
+	font-size: 18px;
+	margin-left: 10px;
+`;
+
+export const HomeIcon = styled(OpticonIcon)``;
+
+export const AccountIcon = styled(MaterialCommunityIcon)``;
+
+export const GalleryIcon = styled(IonIcon)``;
+
+export const LogutIcon = styled(IonIcon)``;
