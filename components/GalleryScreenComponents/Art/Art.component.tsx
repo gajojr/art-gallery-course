e8beb0880b29c0d547581f404c2be6c40ac8edb7
gajojr <andrijagajicbusiness@gmail.com>
2023-12-23 23:55:27 +0100

    chore: add art comopnent on gallery screen

---
+++ components/GalleryScreenComponents/Art/Art.component.tsx
@@ -0,0 +1,100 @@
+import {
+	FeaturedArtWrapper,
+	ArtImage,
+	ImagesWrapper,
+	LinkWrapper,
+	SectionTitle,
+	SoldArtWrapper,
+	SoldOverlay,
+	Wrapper,
+} from './Art.style';
+import { useFonts, Questrial_400Regular } from '@expo-google-fonts/questrial';
+import { getFirestore, collection, query, getDocs } from 'firebase/firestore';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/store';
+
+const Art = () => {
+	const [images, setImages] = useState<
+		{
+			currentOwner: string;
+			creator: string;
+			href: string;
+			src: {
+				uri: string;
+			};
+		}[]
+	>([]);
+	const [loaded, error] = useFonts({
+		Questrial_400Regular,
+	});
+
+	useEffect(() => {
+		const fetchArt = async () => {
+			const db = getFirestore();
+			const q = query(collection(db, 'art'));
+
+			const querySnapshot = await getDocs(q);
+			const storage = getStorage();
+
+			const imageUrls = await Promise.all(
+				querySnapshot.docs.map(async (doc) => {
+					const { imageUrl, currentOwner, creator } = doc.data();
+					const imageRef = ref(storage, imageUrl);
+					const downloadUrl = await getDownloadURL(imageRef);
+					return {
+						currentOwner,
+						creator,
+						href: downloadUrl,
+						src: {
+							uri: downloadUrl,
+						},
+					};
+				})
+			);
+
+			setImages(imageUrls);
+		};
+
+		fetchArt().catch(console.error);
+	}, []);
+
+	return (
+		<Wrapper>
+			<FeaturedArtWrapper>
+				<SectionTitle>Featured</SectionTitle>
+				<ImagesWrapper>
+					{images
+						.filter((image) => image.currentOwner === image.creator)
+						.map((image, index) => (
+							<LinkWrapper
+								key={index}
+								onPress={() => console.log(image.href)}
+							>
+								<ArtImage source={image.src} />
+							</LinkWrapper>
+						))}
+				</ImagesWrapper>
+			</FeaturedArtWrapper>
+			<SoldArtWrapper>
+				<SectionTitle>Sold</SectionTitle>
+				<ImagesWrapper>
+					{images
+						.filter((image) => image.currentOwner !== image.creator)
+						.map((image, index) => (
+							<LinkWrapper
+								key={index}
+								onPress={() => console.log(image.href)}
+							>
+								<ArtImage source={image.src} />
+								<SoldOverlay
+									source={require('../../../assets/images/sold.png')}
+								/>
+							</LinkWrapper>
+						))}
+				</ImagesWrapper>
+			</SoldArtWrapper>
+		</Wrapper>
+	);
+};
---
+++ components/GalleryScreenComponents/Art/Art.style.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.View`
+	width: 100%;
+	align-items: center;
+	margin-top: 20px;
+`;
+
+export const FeaturedArtWrapper = styled.View`
+	width: 90%;
+`;
+
+export const SoldArtWrapper = styled.View`
+	width: 90%;
+`;
+
+export const SectionTitle = styled.Text`
+	color: #fff;
+	font-size: Questrial_400Regular;
+	font-size: 22px;
+	margin-top: 5px;
+`;
+
+export const ImagesWrapper = styled.View`
+	flex-direction: row;
+	overflow-x: scroll;
+	margin-top: 10px;
+	margin-bottom: 10px;
+`;
+
+export const LinkWrapper = styled.TouchableOpacity`
+	margin-top: 10px;
+`;
+
+export const ArtImage = styled.Image.attrs({
+	testID: 'ArtImage',
+})`
+	width: 170px;
+	height: 170px;
+	border-radius: 10px;
+`;
+
+export const SoldOverlay = styled.Image.attrs({
+	testID: 'SoldOverlay',
+})`
+    position: absolute;
+    width: 50%;
+    height: 50%
+    left: 50%;
+    transform: rotate(30deg);
+`;
